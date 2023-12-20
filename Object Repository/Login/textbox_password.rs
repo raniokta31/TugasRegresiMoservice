c272f05e21772e273a23e5370e4b750a5c6c0b9a
51206151 <51206151@NBLV18100002.adira.co.id>
2023-12-20 12:51:12 +0700

    commit login: verifikasi login username passowrd valid

---
+++ Object Repository/Login/textbox_password.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>textbox_password</name>
+   <tag></tag>
+   <elementGuidId>b7395973-aebe-4c71-8c4e-410c6be59b48</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id=&quot;fieldKataSandi&quot;]/child::div/input</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>

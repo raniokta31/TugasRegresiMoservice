e5da8d5bddbc7bec2355ea527fc371f53a8f0160
51206151 <51206151@NBLV18100002.adira.co.id>
2023-12-22 10:43:58 +0700

    Commit reedem promo mobil: bukan grup bengkel, tanpa jadwal service

---
+++ Object Repository/Reedem Promo Mobil/dropdown_select.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>dropdown_select</name>
+   <tag></tag>
+   <elementGuidId>49725310-1c91-4486-9459-a5ede143670c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=' css-1hwfws3']/div[text()='Select...']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Reedem Promo Mobil/dropdown_selected.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>dropdown_selected</name>
+   <tag></tag>
+   <elementGuidId>f8b15376-5b91-455a-8086-fdd8bf9efe50</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[text() = 'BMW - B2021AB']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
